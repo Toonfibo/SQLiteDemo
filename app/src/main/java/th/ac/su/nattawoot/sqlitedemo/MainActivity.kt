b80eddb0ef3b70aea9afca34ae7b80dd1b51a6e8
Toonfibo <toonbaba.gg@gmail.com>
2020-02-17 10:53:30 +0700

    start

---
+++ app/src/main/java/th/ac/su/nattawoot/sqlitedemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.nattawoot.sqlitedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
