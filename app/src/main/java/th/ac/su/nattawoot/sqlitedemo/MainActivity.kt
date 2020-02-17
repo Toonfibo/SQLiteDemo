24dfeed7f5ed2e2eb1b32c8615a2c655d20c0963
Toonfibo <toonbaba.gg@gmail.com>
2020-02-17 16:07:45 +0700

    finish

--- app/src/main/java/th/ac/su/nattawoot/sqlitedemo/MainActivity.kt
+++ app/src/main/java/th/ac/su/nattawoot/sqlitedemo/MainActivity.kt
@@ -2,33 +2,42 @@ package th.ac.su.nattawoot.sqlitedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 import th.ac.su.nattawoot.sqlitedemo.adapter.ListStudentAdapter
+import th.ac.su.nattawoot.sqlitedemo.dbhelper.DBHelper
 import th.ac.su.nattawoot.sqlitedemo.model.Student
 
 class MainActivity : AppCompatActivity() {
     var listStudent= ArrayList<Student>()
     lateinit var  listViewStudent:ListView
-
+    lateinit var  dbHelper: DBHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var student1 = Student( 1 , "toon" ,  "toonbaba1.gg@gmail.com" )
-        var student2 = Student(2 , "oon" ,  "toonbaba2.gg@gmail.com" )
-        var student3 = Student( 3 , "oo" , "toonbaba3.gg@gmail.com" )
-        listStudent.add(student1)
-        listStudent.add(student2)
-        listStudent.add(student3)
 
+        dbHelper = DBHelper(this)
 
         listViewStudent =  findViewById<ListView>(R.id.listViewStudent)
+
+        val btnAdd = findViewById<Button>(R.id.btnAdd)
+        val edtId = findViewById<EditText>(R.id.edtId)
+        val edtName = findViewById<EditText>(R.id.edtName)
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
+        btnAdd.setOnClickListener {
+            val student = Student(edtId.text.toString().toInt(),edtName.text.toString(),edtEmail.text.toString())
+            dbHelper.addStudent(student)
+            reloadData()
+        }
 
         reloadData()
 
 
     }
     fun reloadData(){
+        listStudent = dbHelper.allStudent
         val adpter = ListStudentAdapter(this@MainActivity,listStudent)
         listViewStudent.adapter = adpter
 
--- app/src/main/java/th/ac/su/nattawoot/sqlitedemo/dbhelper/DBHelper.kt
+++ app/src/main/java/th/ac/su/nattawoot/sqlitedemo/dbhelper/DBHelper.kt
@@ -1,7 +1,11 @@
 package th.ac.su.nattawoot.sqlitedemo.dbhelper
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import th.ac.su.nattawoot.sqlitedemo.model.Student
+
 class DBHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
     companion object{
         private val DATABASE_VERSION = 1
@@ -13,13 +17,46 @@ class DBHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DA
         private val COL_EMAIL = "email"
 
     }
+    //contstand จะใช้ตัวใหญ่ในการประกาศตัวแปล
 
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val CREATE_TABLE =("CREATE TABLE $TABLE_NAME($COL_ID INTEGER PRIMARY KEY , $COL_NAME TEXT ,$COL_EMAIL TEXT)")
+        db!!.execSQL(CREATE_TABLE)
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-    //contstand จะใช้ตัวใหญ่ในการประกาศตัวแปล
+    val allStudent:ArrayList<Student>
+        get(){
+            val  listStudent = ArrayList<Student>()
+            val  SELECT_QUREY = "SELECT * FROM $TABLE_NAME"
+            val db:SQLiteDatabase = this.writableDatabase
+            val cursor: Cursor = db.rawQuery(SELECT_QUREY,null)
+            if (cursor.moveToFirst()){
+                do {
+
+                    val student = Student()
+                    student.id = cursor.getInt(cursor.getColumnIndex(COL_ID))
+                    student.name = cursor.getString(cursor.getColumnIndex(COL_NAME))
+                    student.email = cursor.getString(cursor.getColumnIndex(COL_EMAIL))
+
+                    listStudent.add(student)
+
+                }while (cursor.moveToNext())
+            }
+
+            return listStudent
+        }
+    fun addStudent(student: Student){
+        val db:SQLiteDatabase =this.writableDatabase
+        val contentValue = ContentValues()
+        contentValue.put(COL_ID,student.id)
+        contentValue.put(COL_NAME,student.name)
+        contentValue.put(COL_EMAIL,student.email)
+        db.insert(TABLE_NAME,null,contentValue)
+        db.close()
+
+
+    }
 }
