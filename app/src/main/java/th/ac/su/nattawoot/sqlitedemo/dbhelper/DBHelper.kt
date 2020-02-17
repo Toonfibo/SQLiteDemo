8321226a213d78858d28b04cc20a88e7718f303b
Toonfibo <toonbaba.gg@gmail.com>
2020-02-17 15:04:21 +0700

    DBHelper_Update

--- app/src/main/java/th/ac/su/nattawoot/sqlitedemo/dbhelper/DBHelper.kt
+++ app/src/main/java/th/ac/su/nattawoot/sqlitedemo/dbhelper/DBHelper.kt
@@ -1,4 +1,25 @@
 package th.ac.su.nattawoot.sqlitedemo.dbhelper
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+class DBHelper(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+    companion object{
+        private val DATABASE_VERSION = 1
+        private val DATABASE_NAME = "University.db"
 
-class DBHelper {
+        private val TABLE_NAME = "student"
+        private val COL_ID = "id"
+        private val COL_NAME = "name"
+        private val COL_EMAIL = "email"
+
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    //contstand จะใช้ตัวใหญ่ในการประกาศตัวแปล
 }
